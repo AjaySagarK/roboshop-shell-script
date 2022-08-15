40b08fc15b81357feed1c66964ec3adc7de86b55
AjaySagarK <ajaysagarkaradi13@gmail.com>
2022-08-15 22:23:02 +0530

    updating ./roboshop-shell-script

--- components/frontend.sh
+++ components/frontend.sh
@@ -15,5 +15,4 @@ mv frontend-main/static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
-
 systemctl restart nginx
