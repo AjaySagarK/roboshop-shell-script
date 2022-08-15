245eb665b414fd9686de09aafce021db3916d757
AjaySagarK <ajaysagarkaradi13@gmail.com>
2022-08-15 22:30:10 +0530

    updating ./roboshop-shell-script

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
@@ -7,7 +8,7 @@ systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
 
-cd /usr/share/nginx/html
+cd  /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
 mv frontend-main/* .
