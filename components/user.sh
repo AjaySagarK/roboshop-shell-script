6e83e6440166a9db12fe83d2b01e16c194aeb5cf
AjaySagarK <ajaysagarkaradi13@gmail.com>
2022-08-15 21:30:34 +0530

    updating ./roboshop-shell-script

--- components/user.sh
+++ components/user.sh
@@ -1,1 +1,26 @@
-echo install User Component
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+
+useradd roboshop
+
+curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
+cd /home/roboshop
+unzip /tmp/user.zip
+mv user-main user
+cd /home/roboshop/user
+npm install
+
+#1. Update SystemD service file,
+    
+  #  Update `REDIS_ENDPOINT` with Redis Server IP
+    
+   # Update `MONGO_ENDPOINT` with MongoDB Server IP
+    
+#2. Now, lets set up the service with systemctl.
+
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/user/systemd.service
+
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+ systemctl daemon-reload
+ systemctl start user
+ systemctl enable user
